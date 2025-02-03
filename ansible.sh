654fe9f0307d107cdd2e140283a5dce43a396867
Snehil Shubham <snehil1693@gmail.com>
2025-02-03 15:13:11 +0530

    ansible installation

---
+++ ansible.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Server Update"
+sudo apt update -y
+
+echo "###############################"
+
+echo "Server Upgrade"
+sudo apt upgrade -y
+
+echo "###############################"
+
+echo "Add Ansible PPA"
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+
+echo "###############################"
+
+echo "Install Ansible"
+sudo apt install ansible -y
+
+echo "###############################"
+
+echo "ansible version"
+ansible -version
